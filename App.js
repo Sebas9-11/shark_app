4576a1bfafc6a7244b9bfedc961784a5baedc288
Sebas9-11 <sebastian-1999@outlook.com>
2022-11-01 09:48:41 -0500

    only commit

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import Index from "./src/navigation/Index";
 import GroupsView from "./src/screens/judgesProfiles/groupsView/GroupsView";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return <Index />;
--- app.config.js
+++ app.config.js
@@ -1,44 +1,42 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-export default{
-  "expo": {
-    "name": "shark_app",
-    "slug": "shark_app",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+export default {
+  expo: {
+    name: "shark_app",
+    slug: "shark_app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "package": "com.sebas911.shark_app",
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      }
+    android: {
+      package: "com.sebas911.shark_app",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    extra:{
-      apiKey : process.env.API_KEY,
-      authDomain : process.env.AUTH_DOMAIN,
-      projectId : process.env.PROJECT_ID,
-      storageBucket : process.env.STORAGE_BUCKET,
-      messagingSenderId : process.env.MESSAGING_SENDER_ID,
-      appId : process.env.APP_ID,
-    }
-  }
-}
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+    },
+  },
+};
